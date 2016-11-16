a6517ebbfd04968baf64c5189f75fb35f2b0c312
Peter Doan <pdoan017@uottawa.ca>
2016-11-15 19:37:33 -0500

    Added my student number to hand files. Added insertion operator funciton that overrides default (We haven't learned about this yet in class ).

--- gemstone/gemstone/hand.hpp
+++ gemstone/gemstone/hand.hpp
@@ -2,7 +2,7 @@
 //  Project : gemstone
 //  File    : card-hierarchy.hpp
 //  Student : Ali Sanaknaki (7745880)
-//  Student : Peter Doan (#######)
+//  Student : Peter Doan (6842312)
 //
 //  Github  : http://www.github.com/sanaknaki/gemstone
 // ==========================================================================
@@ -17,20 +17,28 @@
 #include "gemstones.hpp"
 #include "cardfactory.hpp"
 
-using std::istream; using std::vector;
+using std::istream; using std::ostream; using std::vector;
 
-class Hand{
+class Hand {
     vector<Card*> d_cards;
-    
-private:
+
+protected:
 
 public:
-    Hand(const istream&,CardFactory*);
+    // Hand needs a constructor which accepts an istream and reconstructs the Hand from file.
+    Hand( const istream&, CardFactory* );
     virtual ~Hand();
-    Hand& operator+=(Card* _card);
+    // adds the card to the rear of the hand
+    Hand& operator+=( Card* _card );
+    // returns and removes the top card from the player's hand
     Card* play();
+    // returns but does not remove the top card from the player's hand
     Card* top();
-    Card* operator[](int i);
+    // insertion operator to print Hand on an std::ostream
+    // the hand should print all the cards in order
+    friend ostream& operator<<( ostream&, const Hand& );
+    // returns and removes the Card at a given index
+    Card* operator[]( int i );
 };
 
 #endif /* hand_hpp */
