c317a6d2c9dcf4e21d623bb77831efd181ac40b9
Peter Doan <pdoan017@uottawa.ca>
2016-11-22 18:16:44 -0500

    Finished all except for construction from istream

--- gemstone/gemstone/trade_area.hpp
+++ gemstone/gemstone/trade_area.hpp
@@ -14,25 +14,25 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <list>
 
 #include "gemstones.hpp"
 #include "cardfactory.hpp"
 
-using std::istream;
+using std::istream; using std::list;
 
 class TradeArea {
-
-protected:
+    list<Card*> d_cards;
 
 public:
     // constructor which accepts an istream and reconstruct the TradeArea from file
-    TradeArea( const istream& _is, CardFactory* _cardPool);
+    TradeArea( const istream& _is, CardFactory* _cardPool );
     // adds the card to the trade area but it does not check if it is legal to place the card
-    TradeArea& operator+=( Card* _card);
+    TradeArea& operator+=( Card* _card );
     // returns true if the card can be legally added to the TradeArea, i.e., a card of the same gemstone is already in the TradeArea
     bool legal( Card* _card );
     // removes a card of the corresponding gemstone name from the trade area
-    Card trade( string _gemName );
+    Card* trade( string _gemName );
     // returns the number of cards currently in the trade area
     int numCards();
 };
